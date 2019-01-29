da8dcc2a6008ff8ef5425d435146f291bf13fba6
Gustavo Leste <gustavoleste@protonmail.com>
2019-01-29 16:02:57 -0200

    Criação do componente Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+
+const footer = () => (
+          <Container>
+                    <Content>
+                              <p>Feito com <FontAwesomeIcon icon={faHeart} color='#ff0000' /> por Gustavo Leste</p>
+                              <Logos>
+                                        <FontAwesomeIcon icon={faGithub} color='#fff' size='lg'/> <FontAwesomeIcon icon={faLinkedin} color='#fff' size='lg'/> 
+                              </Logos>
+                    </Content>
+          </Container>
+)
+
+export default footer
+
+const Container = styled.footer`
+          background: #000;
+`
+const Content = styled.div`
+          color: #fff;
+          padding: 1rem 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          
+          p{
+                    margin-bottom: 0.5rem;
+          }
+
+          @media (min-width: 1024px){
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
+
+                    p{
+                              margin:0;
+                              font-size: 1.5rem;
+                    }
+          }
+`
+const Logos = styled.div`
+
+          svg{
+                    margin: 0 0.5rem;
+          }          
+`
