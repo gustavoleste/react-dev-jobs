3ed5888ead858445ea23c907f7068ca8744c8f74
Gustavo Leste <gustavoleste@protonmail.com>
2019-02-12 14:33:46 -0200

    Updates

---
+++ precache-manifest.a447703a4b8baacb8067af1682bf186e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "49f88bae62c54c45944b",
+    "url": "/react-dev-jobs/static/js/runtime~main.49f88bae.js"
+  },
+  {
+    "revision": "4bb7f6af7a579d70a152",
+    "url": "/react-dev-jobs/static/js/main.4bb7f6af.chunk.js"
+  },
+  {
+    "revision": "c14f65b38ea61ae81a79",
+    "url": "/react-dev-jobs/static/js/1.c14f65b3.chunk.js"
+  },
+  {
+    "revision": "bee0b9ae893ae326fa784a5b45b14196",
+    "url": "/react-dev-jobs/index.html"
+  }
+];
